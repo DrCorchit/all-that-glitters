f5a240d8d5b36721f225493d24786c62d6153828
DrCorchit <drcorchit@gmail.com>
2024-08-04 21:52:21 -0400

    boogfeex

--- src/components/Outline.tsx
+++ src/components/Outline.tsx
@@ -1,7 +1,7 @@
 import {normalize} from "../utils/utils";
 import {HashLink} from "react-router-hash-link";
 
-export default function Outline({pathname, sections}: {pathname?: string; sections: string[]}) {
+export default function Outline({pathname = "", sections}: {pathname?: string; sections: string[]}) {
 	const links = sections.map((name, index) => {
 		const link = `${pathname}#${normalize(name)}`;
 		return (
