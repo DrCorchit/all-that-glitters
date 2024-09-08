008765208143943987e998356407dafe04591f10
DrCorchit <drcorchit@gmail.com>
2024-09-08 17:58:09 -0400

    Chunking fix

--- vite.config.ts
+++ vite.config.ts
@@ -9,4 +9,15 @@ export default defineConfig({
 		port: 5173,
 		strictPort: true,
 	},
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks(id) {
+					if (id.includes("node_modules")) {
+						return id.toString().split("node_modules/")[1].split("/")[0].toString();
+					}
+				},
+			},
+		},
+	},
 });
